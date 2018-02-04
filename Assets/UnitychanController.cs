70e6e4dfb8fd2e962e80f5ea185451f5a1d35e7c
Administrator <ryo.elevensoul@gmail.com>
2018-02-04 20:41:15 +0900

    4.5 ユニティちゃんが前進するスクリプトを作成

--- Assets/UnitychanController.cs
+++ Assets/UnitychanController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class UnitychanController : MonoBehaviour {
 	// アニメーションするためのコンポーネント
 	private Animator myAnimator;
+	// Unityちゃんを移動させるコンポーネントを入れる
+	private Rigidbody myRigidbody;
+	// 前進するための力
+	private float forwardForce = 800.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,10 +16,13 @@ public class UnitychanController : MonoBehaviour {
 		this.myAnimator = GetComponent<Animator>();
 		// 走るアニメーションを開始
 		this.myAnimator.SetFloat("Speed", 1.0f);
+		// Rigidbodyコンポーネントを取得
+		this.myRigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// Unityちゃんに前方向の力を加える
+		this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
 	}
 }
