40835d830722a865bf0959717779cd288d0eb933
Administrator <ryo.elevensoul@gmail.com>
2018-02-04 20:35:31 +0900

    4.4 ユニティちゃんに走るアニメーションをさせる

---
+++ Assets/UnitychanController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitychanController : MonoBehaviour {
+	// アニメーションするためのコンポーネント
+	private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+		// Animatorコンポーネントを取得
+		this.myAnimator = GetComponent<Animator>();
+		// 走るアニメーションを開始
+		this.myAnimator.SetFloat("Speed", 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
